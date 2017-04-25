dff3a810797d3d9c0ffda6bdfcf523251b3b33d9
iliandy <iandyli@gmail.com>
2017-04-25 12:34:30 -0500

    initial commit w/ server settings

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'il+wt0%pq&fdrkbejb%!-f1p648yfupvjq!-zl=th7+w7+ru=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
