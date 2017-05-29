cea0a4d56f3b9b6ea5f742809c7dfd6877b87cbb
iliandy <iandyli@gmail.com>
2017-05-28 22:49:24 -0500

    recreating GitHub repo

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'il+wt0%pq&fdrkbejb%!-f1p648yfupvjq!-zl=th7+w7+ru=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
